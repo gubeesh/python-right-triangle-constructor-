2905455d993998991004c06240c7546a574ece32
gubeesh <124607955+gubeesh@users.noreply.github.com>
2023-02-07 14:29:56 +0000

    Update right-triangle constructor.py
    works as long as "x" is not 1

--- right-triangle constructor.py
+++ right-triangle constructor.py
@@ -8,7 +8,7 @@ c=x*x+1
 turtle.forward(b)
 turtle.right(90)
 turtle.forward(a)
-#some trigonometry 
+#some trigonometry stuff
 #the function math.asin returns the values in radians, where in 1 radian = 57.2958 degrees
 x = math.asin(b/c)*57.2958
 turtle.right(180-x)
