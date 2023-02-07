779e41c7b255fd43fa5a4b017648e037de639f03
gubeesh <124607955+gubeesh@users.noreply.github.com>
2023-02-07 12:33:16 +0000

    Update right-triangle constructor.py

--- right-triangle constructor.py
+++ right-triangle constructor.py
@@ -1,13 +1,14 @@
 import turtle
 import math
 x = float(input("enter a number:"))
-#
-a=2x
+#uses pythagorean triplet formula to find them
+a=2*x
 b=x*x-1  
 c=x*x+1 
 turtle.forward(b)
 turtle.right(90)
 turtle.forward(a)
+#some trigonometry 
 #the function math.asin returns the values in radians, where in 1 radian = 57.2958 degrees
 x = math.asin(b/c)*57.2958
 turtle.right(180-x)
