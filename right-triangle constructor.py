792a0a9fa21f0776539de5d5a8eae8b4bde4ee3c
gubeesh <124607955+gubeesh@users.noreply.github.com>
2023-03-25 16:42:33 +0530

    added support for isoceles right triangles.

--- right-triangle constructor.py
+++ right-triangle constructor.py
@@ -1,12 +1,23 @@
 import turtle
 import math
 x = abs(float(input("enter a number:")))
-a=x
-b=abs(x*x/4-1)
-c=x*x/4+1 
-turtle.forward(b)
-turtle.left(90)
-turtle.forward(a)
-x = math.asin(b/c)*57.2958
-turtle.left(180-x)
-turtle.forward(c)
+y = input("should the constructed right-triangle be isoceles?(y if yes, leave if no)?: ")
+
+
+if y == "y":
+    turtle.forward(x)
+    turtle.right(135)
+    turtle.forward(math.sqrt(2)*x)
+    turtle.right(135)
+    turtle.forward(x)
+
+else:
+    a=x
+    b=abs(x*x/4-1)
+    c=x*x/4+1 
+    turtle.forward(b)
+    turtle.left(90)
+    turtle.forward(a)
+    x = math.asin(b/c)*57.2958
+    turtle.left(180-x)
+    turtle.forward(c)
